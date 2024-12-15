852ccc362cf70d5daf5b3108a37aa266f1ca4d97
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-12-15 16:57:22 +0600

    add category list

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import Banner from "./Banner/Banner"
+import Categories from "./Categories/Categories"
 
 const Home = () => {
     return (
         <div>
 
             <Banner></Banner>
+
+            <Categories></Categories>
 
         </div>
     )
